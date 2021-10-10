7d17386417a973f4d307fd3e2f53a1700a23e313
ACCFernando <fernando.candalaft@gmail.com>
2021-10-10 12:00:37 -0300

    returns

--- create_database.py
+++ create_database.py
@@ -1,25 +1,26 @@
 from pandas_datareader import data as web
 import pandas as pd
 
-def load_tckr(ticker, start, end):
+def load_tckr(ticker, start, end): # load stock history dataset from API
     df = web.DataReader(name=ticker, data_source='yahoo', start=start, end=end).reset_index()
     
     return df
 
-def result_dia(x):
+def result_dia(x): #classify wins, losses and draws
     if x == 0: return 0
     if x < 0: return 1
     if x > 0: return 2
 
-def enrich(df):
+def enrich(df): #enrich data with useful indexes
     lower = [str.lower(c) for c in df.columns.to_list()]
     df.columns = lower
-    df['return'] = df.open - df.close #daily movement
+    df['return'] = df.close - df.open #daily movement
+    df['pct_change'] = 100*df['return']/df['open']
     df['result_dia'] = df['return'].apply(result_dia)
     
     return df
 
-def get_ticker_list(ticker_path):
+def get_ticker_list(ticker_path):#create stock ticker list from file 
     header = ['ticker', 'company', 'tp', 'theory_qnt', 'part'] 
     tick = pd.read_excel(ticker_path, names=header)
     t = tick['ticker'].to_numpy()
@@ -28,7 +29,7 @@ def get_ticker_list(ticker_path):
     
     return l_tick
 
-def create_database(start,end, ticker_path):
+def create_database(start,end, ticker_path): #wrap up
     l_tick = get_ticker_list(ticker_path)
     df = pd.DataFrame()
 
